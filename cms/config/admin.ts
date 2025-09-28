bb2db08c48a6df6411c4ddffee75b2460d9460ce
Ebrahim Khodadadi <ebrahimkhodadadideveloper@gmail.com>
2025-09-28 08:55:30 +0330

    set proxy in server.ts

--- cms/config/admin.ts
+++ cms/config/admin.ts
@@ -25,6 +25,5 @@ export default ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
-  proxy: { koa: true },
   url: env('ADMIN_URL', '/admin'),
 });
