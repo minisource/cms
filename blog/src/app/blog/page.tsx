a16fd7c173b98f93ba7b0dada1f84b06976a2097
bitashahsavan1396@gmail.com <bitashahsavan1396@gmail.com>
2025-09-30 12:25:41 +0330

    update text of log

--- blog/src/app/blog/page.tsx
+++ blog/src/app/blog/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import data from './api.json'
 
 export default function BlogPage() {
+  console.log(data.data[0].slug)
   return (
     <main className="p-8 text-center">
-      <h1 className="text-3xl font-bold mb-4">بلاگ</h1>
-      <p className="text-gray-600">اینجا لیست مقالات نمایش داده خواهد شد.</p>
+      <h1 className="text-3xl font-bold mb-4">blogs</h1>
+      <p className="text-gray-600">it is blog page</p>
     </main>
   );
 }
